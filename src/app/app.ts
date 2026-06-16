b61b32856bedaa42fe290de2a0694041a709a2af
Alexis28J <jordy2806@hotmail.com>
2026-06-16 14:09:52 +0200

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header-component/header-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('GMail-clone');
---
+++ src/app/layout/mainpage-component/mainpage-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainpage-component',
+  imports: [],
+  templateUrl: './mainpage-component.html',
+  styleUrl: './mainpage-component.scss',
+})
+export class MainpageComponent {}
